40cfff4f5f2a36dc9129a438027d44b5a103ec64
Michael Squires <sqmk@php.net>
2013-11-17 22:57:23 -0800

    GitHub #19 - Fix copyright

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Glovebox: Lightweight Dependency Injection Container
+ *
+ * @author    Michael Squires <sqmk@php.net>
+ * @copyright Copyright (c) 2012-2013 Michael K. Squires
+ * @license   http://github.com/sqmk/Glovebox/wiki/License
+ */
 
 $autoloader = require __DIR__ . '/../vendor/autoload.php';
 $autoloader->add('GloveboxTest', __DIR__);
